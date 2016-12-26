3187282ee755a710eedee43784bf1c795b749489
BorislavShekerov <shekerovborislav@gmail.com>
2016-12-26 11:00:14 +0000

    Added scss support + header-nav-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderNavComponent } from './components/header-nav/header-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavComponent
   ],
   imports: [
     BrowserModule,
