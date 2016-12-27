6c8265899b707e13d204d4d8755058c897c8bde2
BorislavShekerov <shekerovborislav@gmail.com>
2016-12-27 18:37:22 +0000

    Added Filter Panel

---
+++ src/app/services/custom-language-datepicker.service.ts
@@ -0,0 +1,30 @@
+import { NgbDatepickerI18n } from '@ng-bootstrap/ng-bootstrap';
+import { Injectable } from '@angular/core';
+
+const I18N_VALUES = {
+    bg:{
+        weekdays: ['Понеделник', 'Вторник', 'Сряда', 'Четвъртък', 'Петък', 'Събота', 'Неделя'],
+        months: ['Януари', 'Февруари', 'Март', 'Април', 'Май', 'Юни', 'Юли', 'Август', 'Септември', 'Октомври', 'Ноември', 'Декември'],
+    }
+};
+
+@Injectable()
+export class I18n {
+    language = 'bg';
+}
+
+// Define custom service providing the months and weekdays translations
+@Injectable()
+export class CustomDatepickerI18n extends NgbDatepickerI18n {
+
+    constructor(private _i18n: I18n) {
+        super();
+    }
+
+    getWeekdayName(weekday: number): string {
+        return I18N_VALUES[this._i18n.language].weekdays[weekday - 1];
+    }
+    getMonthName(month: number): string {
+        return I18N_VALUES[this._i18n.language].months[month - 1];
+    }
+}
